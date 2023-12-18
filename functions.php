6e4460d15833138941fa5e7c4f3bc503673648ac
IRobotFR <141443104+IRobotFR@users.noreply.github.com>
2023-12-18 14:56:23 +0100

    Update fonctions php
    Simplification de la fonction pour changer le menu en fonction du statut du visiteur (connecté, visiteur non connecté).

--- functions.php
+++ functions.php
@@ -35,10 +35,25 @@ function hello_elementor_child_scripts_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
 
+/**
 add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
 function add_extra_item_to_nav_menu( $items, $args ) {
-	var_dump($items);
+
 	$items 	.= '<li><a href="#">Admin</a></li>';
 
     return $items;
 }
+*/
+
+// Conditional Nav Menu
+
+function wpc_wp_nav_menu_args( $args = '' ) {
+if( is_user_logged_in() ) { 
+    $args['menu'] = 'connecte';
+} else { 
+    $args['menu'] = 'Visiteur';
+} 
+    return $args;
+}
+add_filter( 'wp_nav_menu_args', 'wpc_wp_nav_menu_args' );
+
